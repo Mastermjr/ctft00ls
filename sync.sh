2af6f74f5a72f7f80fc8608b0a60eae04b889277
mastermjr <reeves17@purdue.edu>
2019-03-11 00:54:03 -0400

    adding in peda stuff

--- sync.sh
+++ sync.sh
@@ -4,6 +4,6 @@ git submodule foreach git checkout master; git pull origin master;
 #setup peda
 if [ ! -f ~/.gdbinit ];
   then  
-  echo "source ~/peda/peda.py" >> ~/.gdbinit
+  echo "source ~/ctf/tools/peda/peda.py " >> ~/.gdbinit
   echo "DONE! debug your program with gdb and enjoy"
 fi
