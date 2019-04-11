fed5bd1b00af62cb896ea2c9bd3cb625f6c25501
mastermjr <mastermjr@prontonmail.com>
2019-04-11 10:56:23 -0400

    adding in submodules

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ git submodule foreach git checkout master; git pull origin master;
 #setup peda
 if [ ! -f ~/.gdbinit ];
   then  
-  echo "source ~/ctf/tools/peda/peda.py " >> ~/.gdbinit
+  echo "source ~/ctf/ctft00ls/peda/peda.py " >> ~/.gdbinit
   echo "DONE! debug your program with gdb and enjoy"
 fi
 
